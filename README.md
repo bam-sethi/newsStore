9bcf9c4a25b1d7db61f4b5fdf25cfb180f19a2e3
Ambar Sethi <ambarrrsethi@gmail.com>
2016-04-18 16:12:43 +0100

    adding read me instructions

---
+++ README.md
@@ -0,0 +1,16 @@
+# Offline / Online News Store
+
+This app generates fake news data from using Node.Js and Mongo server
+Fake News Data (Lorem Ipsum) generated using Faker.Js 
+
+#Set up Instructions
+
+In command line:
+
+Run:  `npm install`
+
+Start up the Mongo Server : `mongod`
+
+
+
+
--- app.js
+++ app.js
@@ -2,24 +2,13 @@ var express = require('express');
 var mongoose = require('mongoose');
 var fs = require('fs');
 var path = require('path');
-var bodyParser = require('body-parser');
 var app = express();
 var faker = require('faker')
-
-app.use(express.static(__dirname + '/app'))
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-mongoose.connect('mongodb://localhost:27017/news_data_store');
-
 require("./models/news");
 var News = mongoose.model("News");
 
-// root route
-app.get('/', function(req, res){
-  console.log("hello world")
-  res.sendFile(path.join(__dirname + 'app/index.html'))
-})
-
+app.use(express.static(__dirname + '/app'))
+mongoose.connect('mongodb://localhost:27017/news_data_store');
 
 
 app.get('/news', function(req, res){
@@ -29,10 +18,10 @@ app.get('/news', function(req, res){
     } else {
       res.json(data)
     }
-  })
-})
+  });
+});
 
-var imagesArray = ['images/1.jpg', 'images/2.jpg','images/3.jpg','images/4.jpg','images/5.jpg','images/6.jpg','images/7.jpg','images/8.jpg', 'images/9.jpg', 'images/10.jpg']
+var imagesArray = ['images/1.jpg', 'images/2.jpg','images/3.jpg','images/4.jpg','images/5.jpg','images/6.jpg','images/7.jpg','images/8.jpg', 'images/9.jpg', 'images/10.jpg'];
 
 
 function createNewsItem(){
@@ -43,15 +32,13 @@ function createNewsItem(){
     description: faker.lorem.sentences(),
     image: imagesArray[n]
   });
-  // console.log(news)
   news.save(function(err, news) {
     if (err) console.log(err);
-    // console.log('NEWS Saved!');
   });
 }
 
 for(var i =0; i < 10; i++){
-  createNewsItem()
+  createNewsItem();
 }
 
 app.listen(3000);
